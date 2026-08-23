202bacf6b7f1d364f4b3533bcfbd8573963f51ac
JkhTV <candzzang5283@gmail.com>
2026-08-23 14:09:23 +0900

    Upload broken code

---
+++ .py
@@ -0,0 +1,185 @@
+from asyncio import FastChildWatcher, wait_for
+from os import WNOWAIT, fwalk
+from re import A, S
+from socket import AF_AAL5
+from tkinter import W
+from typing import ForwardRef
+from wave import Wave_write
+
+
+aeofo eaww 
+f 
+fwa fp w  ForwardRefwf wait_forwaf
+awf 
+wafwa
+ 
+ finallyf
+  w 
+
+  finallyaw
+  f 
+  wa
+  fwalkf
+  Wf
+  w 
+  fwalkfwf
+  w
+  f
+
+   wait_forf
+   wa
+
+   fwalkw
+
+   Wwiat 
+    Ww
+
+    WNOWAITaw
+    Wave_writew
+    Wave_writew
+    Wave_writew
+    Ww
+    Ww
+    w
+    w
+    w
+    w
+    w
+
+    Wave_writew
+    Wave_writew
+
+
+    FastChildWatcherf Af
+    AF_AAL5faw
+    fwalkawf
+    w
+
+    wait 
+
+    Wfwa
+    f wajfow akf wjao'f AF WJA  
+     ㄹ ㅈ
+     ;ㄹ '
+     ㅁ'ㅁ
+     ㄹ;
+      ''ㄹ
+      ㄷㅁㄹ
+       ;
+       ㄹ;ㅈ
+       ㄹ;
+        ㄹ
+        ㅈㅁ;
+        ㄹ;ㅈ
+        ㅁ'ㄹ
+        ㅈㅁㄹ
+        ㄹ'ㅈㅁ
+        ㄹ;ㄹ;ㄹㅈㅁㄹ
+        ; ㄹ
+        ㅁ
+        ㄹ 'ㄴ
+        ㄹ 
+        ㄹ;ㄴ 
+         ㄹ'
+         ㅁㅈ'ㄴ
+         ' ㄹ
+         'ㄴ
+         ㄹ '
+         'ㄹ
+         ㄴㄹ
+          ㅁㄴ'
+          ㄹ'ㅁ
+          ㄹ'
+          ㅁ ㄹ
+          ㅁ'ㄹ 
+          ㄴㅁ'ㄹ
+          ㄴ'ㄹ
+          ㅁㄴ'ㄹ
+          ㄴㅁㄹ'as
+          f's
+          f'
+          sa'f
+          s'afsaf's
+          'f
+          s'f
+          s'f
+          s'f
+          s'f
+          s'f
+          s'f
+          s'fs
+          'f
+          s'f
+          s'f
+          s'
+          s'
+          s'
+          s'f
+          s'fs
+          'f
+          s'f
+          s'fs's
+          f's
+          f's
+          f's
+          f's
+          f's
+          f's
+          'fs
+          f'
+          s'fs
+          'f
+          s'f
+          s'f
+          s'f
+          s'f
+          s'f
+          s'f
+          s'f
+          s'f
+          s'
+          fs'
+          fs'fs
+          f's
+          f's
+          f'
+          sf'
+          s'f
+          s'f
+          s'
+          fs'
+          fs'
+          f's
+          f's's
+          'f
+          s'f
+          s'
+          s'f
+          '
+          s'
+          s'
+          s'
+          Ss'
+          s'
+          's
+          f's
+          f's
+          f'
+          s'f
+          sf'
+          s'f
+          s'f
+          s'f
+          s'f
+          s'f
+          s'f
+          s'
+          f'
+           '
+            
+            fwa
+             f'
+              맂 레
+               ㅈㅁ
+               ㅏㄹ
+               ㄹ ㄹ ㄻ ㄹㅈㅁㄹ'
